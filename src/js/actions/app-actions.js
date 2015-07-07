c15e19c2728c6c161eab8e7754a09e70bcf2de50
Felix Guerrero <felixcriv@gmail.com>
2015-07-07 12:12:02 -0400

    added EDIT_APPOINTMENT to constants, added editAppointment to actions

---
+++ src/js/actions/app-actions.js
@@ -0,0 +1,20 @@
+var AppDispatcher = require('../dispatchers/app-dispatcher');
+var AppConstants = require('../constants/app-constants');
+
+var AppActions = {
+	createAppointment: function(appointment){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.CREATE_APPOINTMENT,
+			appointment: appointment
+		})
+	},
+
+	editAppointment: function(appointment){
+		AppDispatcher.handleViewAction({
+			actionType: AppConstants.EDIT_APPOINTMENT,
+			appointment: appointment
+		});
+	}
+}
+
+module.exports = AppActions;
