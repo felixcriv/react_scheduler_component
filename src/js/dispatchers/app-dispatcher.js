0781d3699fb56eb24aa550c0282ca2d5cf08db9a
Felix Guerrero <felixcriv@gmail.com>
2015-07-07 22:30:29 -0400

    dispatcher updated

--- src/js/dispatchers/app-dispatcher.js
+++ src/js/dispatchers/app-dispatcher.js
@@ -1,15 +1,12 @@
 var Dispatcher = require('flux').Dispatcher;
-var assign = require('react/lib/Object.assign');
 
-var AppDispatcher = assign(new Dispatcher(), {
+var AppDispatcher = new Dispatcher();
 
-	handleViewAction: function(action){
-		this.dispatch({
-			source: 'VIEW_ACTION',
-			action: action
-		})
-	}
-
-});
+AppDispatcher.handleViewAction = function(action) {
+  this.dispatch({
+    source: 'VIEW_ACTION',
+    action: action
+  });
+}
 
 module.exports = AppDispatcher;
