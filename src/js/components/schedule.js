3fea2c14db7419b3bf74bc3f66856fb494133d17
Felix Guerrero <felixcriv@gmail.com>
2015-07-08 11:59:52 -0400

    updated render function with key/pair approach

--- src/js/components/schedule.js
+++ src/js/components/schedule.js
@@ -16,6 +16,7 @@ var Schedule = React.createClass({
 	},
 
 	componentDidMount: function(){
+		
 		AppStore.addChangeListener(this._onChange);
 	},
 
@@ -31,20 +32,22 @@ var Schedule = React.createClass({
 
 	 this.setState({ showM: true });
 	 this.props.showModal(this.showM);
+	 var editingMode = false;
 
 	 var patient = {};
 
 	 var slot = e.hour+this.props.workingDays[e.day];
 
 	 if(this.refs[slot].getDOMNode().children[0]){
+	 	editingMode = true;
 	 
 	 	patient.name =  this.refs[slot].getDOMNode().children[0].children[0].innerHTML;
-	 	patient.phone = this.refs[slot].getDOMNode().children[0].children[1].innerHTML;
+	 	patient.phone = this.refs[slot].getDOMNode().children[0].children[2].innerHTML;
 
-	 	this.props.update(e, patient);
+	 	//this.props.update(e, patient, editingMode);
 	 }
 	 
-	 this.props.update(e, patient);
+	 this.props.update(e, patient, editingMode);
 	},
 
 	// shouldComponentUpdate: function(nextProps, nextState) {
@@ -52,30 +55,35 @@ var Schedule = React.createClass({
 	// },
 
 	render: function(){
+
+		var days = this.state.appointments[0];
+		var patients = this.state.appointments[1];
       
 		var _slots = function(id, h, handleClick){
 			
 			//react way to do this
 			var daysforSlot = this.props.workingDays.map(function(day, i){
-				
-				var spanActive = this.state.appointments.map(function(appointment){
+				//days with appointments
+				var spanActive = Object.keys(days).map(function(appointment){
 								
 					            var _activeClass = null;
 					            var _s = null;
 
-					            if(h.hour+day === appointment.hour+appointment.day){
+
+					            if(day+h.hour === appointment){
 					            	_activeClass = 'active'
 					            }
 
 					            if(_activeClass){
-					            	_s = <span className='active'>{appointment.patient.name}<p>
-					            	{appointment.patient.phone}
+					            	_s = <span className='active'>{days[appointment]}
+					            	<hr/>
+					            	<p className='hideNumber'>
+					            	{patients[days[appointment]]}
 					            	</p></span>
 					            }
 								
-								
 								return (  {_s}  )
-				});
+				}.bind(this));
 
 				return (
 					<td ref={h.hour+day} className='cell' onClick={handleClick.bind(null, {hour: h.hour, day:i})}>
@@ -104,6 +112,7 @@ var Schedule = React.createClass({
 		}.bind(this));
 
 		return(
+			
 			<span>
 				{_schedule}
 			</span>
