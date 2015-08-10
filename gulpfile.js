94b287f58b4464fffcf96ec5f1acf56e4cf655cd
Felix Guerrero <felixcriv@gmail.com>
2015-08-10 12:57:08 -0400

    gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,23 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('browserify', function(){
-  //source
-  browserify('./src/js/main.js')
-  //convert jsx to js
-    .transform('reactify')
-    //creates a bundle
-    .bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('dist/js'))
+gulp.task('browserify', function() {
+    //source
+    browserify('./src/js/main.js')
+        //convert jsx to js
+        .transform('reactify')
+        //creates a bundle
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('copy', function(){
-    
-	gulp.src('src/index.html')
-	  .pipe(gulp.dest('dist'));
-	gulp.src('src/assets/**/*.*')
-	  .pipe(gulp.dest('dist/assets'));
+gulp.task('copy', function() {
+
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'));
+    gulp.src('src/assets/**/*.*')
+        .pipe(gulp.dest('dist/assets'));
 });
 
 gulp.task('nodemon', function(cb) {
@@ -38,7 +38,7 @@ gulp.task('nodemon', function(cb) {
     });
 });
 
-gulp.task('default', ['browserify', 'copy', 'nodemon'], function(){
+gulp.task('default', ['browserify', 'copy', 'nodemon'], function() {
 
-	return gulp.watch('src/**/*.*', ['browserify', 'copy']);
+    return gulp.watch('src/**/*.*', ['browserify', 'copy']);
 });
